a6e16bb687c95e56d7d086d99d67d7f1c30aa34d
HFO4 <912394456@qq.com>
2020-02-18 13:46:11 +0800

    webdav 目录挂载

--- src/component/Modals/CreateWebDAVMount.js
+++ src/component/Modals/CreateWebDAVMount.js
@@ -1,14 +1,19 @@
-import React, {useEffect, useState} from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core";
 import { Dialog } from "@material-ui/core";
-
+import API from "../../middleware/Api";
 import { useDispatch } from "react-redux";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogActions from "@material-ui/core/DialogActions";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import { FolderOpenOutlined, LabelOutlined } from "@material-ui/icons";
+import FormControl from "@material-ui/core/FormControl";
+import { FolderOpenOutlined, Storage } from "@material-ui/icons";
 import PathSelector from "../FileManager/PathSelector";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import { toggleSnackbar } from "../../actions";
 const useStyles = makeStyles(theme => ({
     formGroup: {
         display: "flex",
@@ -17,7 +22,7 @@ const useStyles = makeStyles(theme => ({
     formIcon: {
         marginTop: 21,
         marginRight: 19,
-        color:theme.palette.text.secondary,
+        color: theme.palette.text.secondary
     },
     input: {
         width: 250
@@ -33,17 +38,24 @@ const useStyles = makeStyles(theme => ({
     }
 }));
 
-export default function CreateWebDAVAccount(props) {
+export default function CreateWebDAVMount(props) {
     const [value, setValue] = useState({
-        name: "",
+        policy: "",
         path: "/"
     });
+    const [policies,setPolicies] = useState([]);
     const [pathSelectDialog, setPathSelectDialog] = React.useState(false);
     const [selectedPath, setSelectedPath] = useState("");
     const [selectedPathName, setSelectedPathName] = useState("");
 
-    const dispatch = useDispatch();
     const classes = useStyles();
+
+    const dispatch = useDispatch();
+    const ToggleSnackbar = useCallback(
+        (vertical, horizontal, msg, color) =>
+            dispatch(toggleSnackbar(vertical, horizontal, msg, color)),
+        [dispatch]
+    );
 
     const setMoveTarget = folder => {
         let path =
@@ -57,17 +69,27 @@ export default function CreateWebDAVAccount(props) {
     const handleInputChange = name => e => {
         setValue({
             ...value,
-            [name]: e.target.value,
+            [name]: e.target.value
         });
     };
 
-    const selectPath = ()=>{
+    const selectPath = () => {
         setValue({
             ...value,
             path: selectedPath === "//" ? "/" : selectedPath
         });
         setPathSelectDialog(false);
-    }
+    };
+
+    useEffect(() => {
+        API.get("/user/setting/policies")
+            .then(response => {
+                setPolicies(response.data.options);
+            })
+            .catch(error => {
+                ToggleSnackbar("top", "right", error.message, "error");
+            });
+    }, []);
 
     return (
         <Dialog
@@ -81,13 +103,21 @@ export default function CreateWebDAVAccount(props) {
                 aria-labelledby="form-dialog-title"
             >
                 <DialogTitle id="form-dialog-title">选择目录</DialogTitle>
-                <PathSelector presentPath="/" selected={[]} onSelect={setMoveTarget} />
+                <PathSelector
+                    presentPath="/"
+                    selected={[]}
+                    onSelect={setMoveTarget}
+                />
 
                 <DialogActions>
                     <Button onClick={() => setPathSelectDialog(false)}>
                         取消
                     </Button>
-                    <Button onClick={selectPath} color="primary" disabled={selectedPath === ""}>
+                    <Button
+                        onClick={selectPath}
+                        color="primary"
+                        disabled={selectedPath === ""}
+                    >
                         确定
                     </Button>
                 </DialogActions>
@@ -96,15 +126,23 @@ export default function CreateWebDAVAccount(props) {
                 <div className={classes.formContainer}>
                     <div className={classes.formGroup}>
                         <div className={classes.formIcon}>
-                            <LabelOutlined />
+                            <Storage />
                         </div>
-
-                        <TextField
-                            className={classes.input}
-                            value={value.name}
-                            onChange={handleInputChange("name")}
-                            label="账户名称"
-                        />
+                        <FormControl className={classes.formControl}>
+                            <InputLabel id="demo-simple-select-label">
+                                存储策略
+                            </InputLabel>
+                            <Select
+                                className={classes.input}
+                                labelId="demo-simple-select-label"
+                                value={value.policy}
+                                onChange={handleInputChange("policy")}
+                            >
+                                {policies.map((v,k)=>(
+                                    <MenuItem key={k} value={v.id}>{v.name}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
                     </div>
                     <div className={classes.formGroup}>
                         <div className={classes.formIcon}>
@@ -115,10 +153,14 @@ export default function CreateWebDAVAccount(props) {
                                 value={value.path}
                                 onChange={handleInputChange("path")}
                                 className={classes.input}
-                                label="相对根目录"
+                                label="目录"
                             />
                             <br />
-                            <Button className={classes.button} color="primary"  onClick={()=>setPathSelectDialog(true)}>
+                            <Button
+                                className={classes.button}
+                                color="primary"
+                                onClick={() => setPathSelectDialog(true)}
+                            >
                                 选择目录
                             </Button>
                         </div>
@@ -127,7 +169,11 @@ export default function CreateWebDAVAccount(props) {
             </div>
             <DialogActions>
                 <Button onClick={props.onClose}>取消</Button>
-                <Button disabled={value.path === "" || value.name === ""} color="primary" onClick={()=>props.callback(value)}>
+                <Button
+                    disabled={value.path === "" || value.policy === ""}
+                    color="primary"
+                    onClick={() => props.callback(value)}
+                >
                     确定
                 </Button>
             </DialogActions>
--- src/component/Setting/WebDAV.js
+++ src/component/Setting/WebDAV.js
@@ -1,7 +1,7 @@
-import React, {useState, useCallback, useEffect} from "react";
-import {makeStyles, Typography} from "@material-ui/core";
-import {useDispatch} from "react-redux";
-import {toggleSnackbar} from "../../actions";
+import React, { useState, useCallback, useEffect } from "react";
+import { makeStyles, Typography } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+import { toggleSnackbar } from "../../actions";
 import Paper from "@material-ui/core/Paper";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
@@ -16,51 +16,59 @@ import Alert from "@material-ui/lab/Alert";
 import Auth from "../../middleware/Auth";
 import API from "../../middleware/Api";
 import IconButton from "@material-ui/core/IconButton";
-import {Delete} from "@material-ui/icons";
+import { Delete } from "@material-ui/icons";
 import CreateWebDAVAccount from "../Modals/CreateWebDAVAccount";
 import TimeAgo from "timeago-react";
+import CreateWebDAVMount from "../Modals/CreateWebDAVMount";
 
 const useStyles = makeStyles(theme => ({
     layout: {
-        width: 'auto',
-        marginTop: '50px',
+        width: "auto",
+        marginTop: "50px",
         marginLeft: theme.spacing(3),
         marginRight: theme.spacing(3),
         [theme.breakpoints.up(1100 + theme.spacing(3) * 2)]: {
             width: 1100,
-            marginLeft: 'auto',
-            marginRight: 'auto',
+            marginLeft: "auto",
+            marginRight: "auto"
         },
-        marginBottom:"50px",
+        marginBottom: "50px"
     },
-    content:{
-        marginTop:theme.spacing(2),
+    content: {
+        marginTop: theme.spacing(4)
     },
-    cardContent:{
-        padding:theme.spacing(2),
+    cardContent: {
+        padding: theme.spacing(2)
     },
-    tableContainer:{
-        overflowX: 'auto',
+    tableContainer: {
+        overflowX: "auto"
     },
-    create:{
-        marginTop:theme.spacing(2),
-    },
+    create: {
+        marginTop: theme.spacing(2)
+    }
 }));
 
-
 export default function WebDAV(props) {
-    const [tab,setTab] = useState(0);
-    const [create,setCreate] = useState(false);
-    const [accounts,setAccounts] = useState([]);
+    const [tab, setTab] = useState(0);
+    const [create, setCreate] = useState(false);
+    const [mount, setMount] = useState(false);
+    const [accounts, setAccounts] = useState([]);
+    const [folders, setFolders] = useState([]);
 
-    useEffect(()=>{
-        API.get("/webdav/accounts").then(response => {
-            setAccounts(response.data.accounts)
-        })
-        .catch(error => {
+    useEffect(() => {
+        loadList();
+    }, []);
+
+    const loadList = () =>{
+        API.get("/webdav/accounts")
+            .then(response => {
+                setAccounts(response.data.accounts);
+                setFolders(response.data.folders);
+            })
+            .catch(error => {
                 ToggleSnackbar("top", "right", error.message, "error");
-        });
-    },[]);
+            });
+    }
 
     const dispatch = useDispatch();
     const ToggleSnackbar = useCallback(
@@ -69,35 +77,71 @@ export default function WebDAV(props) {
         [dispatch]
     );
 
-    const deleteAccount = (id)=>{
+    const deleteAccount = id => {
         let account = accounts[id];
-        API.delete("/webdav/accounts/" + account.ID).then(response => {
-            let accountCopy = [...accounts];
-            accountCopy = accountCopy.filter((v,i) => {
-                return i!==id;
+        API.delete("/webdav/accounts/" + account.ID)
+            .then(response => {
+                let accountCopy = [...accounts];
+                accountCopy = accountCopy.filter((v, i) => {
+                    return i !== id;
+                });
+                setAccounts(accountCopy);
+            })
+            .catch(error => {
+                ToggleSnackbar("top", "right", error.message, "error");
             });
-            setAccounts(accountCopy);
-        }).catch(error => {
-            ToggleSnackbar("top", "right", error.message, "error");
-        });
-    }
+    };
 
-    const addAccount = (account) =>{
+    const deleteMount = id => {
+        let folder = folders[id];
+        API.delete("/webdav/mount/" + folder.id)
+            .then(response => {
+                let folderCopy = [...folders];
+                folderCopy = folderCopy.filter((v, i) => {
+                    return i !== id;
+                });
+                setFolders(folderCopy);
+            })
+            .catch(error => {
+                ToggleSnackbar("top", "right", error.message, "error");
+            });
+    };
+
+    const addAccount = account => {
         setCreate(false);
-        API.post("/webdav/accounts",{
-            path:account.path,
-            name:account.name,
-        }).then(response => {
-            setAccounts([{
-                ID:response.data.id,
-                Password:response.data.password,
-                CreatedAt:response.data.created_at,
-                Name:account.name,
-                Root:account.path,
-            },...accounts]);
-        }).catch(error => {
-            ToggleSnackbar("top", "right", error.message, "error");
-        });
+        API.post("/webdav/accounts", {
+            path: account.path,
+            name: account.name
+        })
+            .then(response => {
+                setAccounts([
+                    {
+                        ID: response.data.id,
+                        Password: response.data.password,
+                        CreatedAt: response.data.created_at,
+                        Name: account.name,
+                        Root: account.path
+                    },
+                    ...accounts
+                ]);
+            })
+            .catch(error => {
+                ToggleSnackbar("top", "right", error.message, "error");
+            });
+    };
+
+    const addMount = mountInfo => {
+        setMount(false);
+        API.post("/webdav/mount", {
+            path: mountInfo.path,
+            policy: mountInfo.policy
+        })
+            .then(response => {
+                loadList();
+            })
+            .catch(error => {
+                ToggleSnackbar("top", "right", error.message, "error");
+            });
     };
 
     const classes = useStyles();
@@ -105,62 +149,166 @@ export default function WebDAV(props) {
 
     return (
         <div className={classes.layout}>
-            <CreateWebDAVAccount callback={addAccount} open={create} onClose={()=>setCreate(false)}/>
-            <Typography color="textSecondary" variant="h4">WebDAV</Typography>
+            <CreateWebDAVAccount
+                callback={addAccount}
+                open={create}
+                onClose={() => setCreate(false)}
+            />
+            <CreateWebDAVMount
+                callback={addMount}
+                open={mount}
+                onClose={() => setMount(false)}
+            />
+            <Typography color="textSecondary" variant="h4">
+                WebDAV
+            </Typography>
             <Paper elevation={3} className={classes.content}>
                 <Tabs
                     value={tab}
                     indicatorColor="primary"
                     textColor="primary"
-                    onChange={(event, newValue)=>setTab(newValue)}
+                    onChange={(event, newValue) => setTab(newValue)}
                     aria-label="disabled tabs example"
                 >
                     <Tab label="账号管理" />
                     <Tab label="存储策略挂载" />
                 </Tabs>
                 <div className={classes.cardContent}>
-                    {tab ===0 &&<div>
-                        <Alert severity="info">WebDAV的登陆用户名统一为：{user.user_name} ；密码为所创建账号的密码。</Alert>
-                        <TableContainer className={classes.tableContainer}>
-                            <Table className={classes.table} aria-label="simple table">
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>账号名称</TableCell>
-                                        <TableCell align="right">密码</TableCell>
-                                        <TableCell align="right">根目录</TableCell>
-                                        <TableCell align="right">创建日期</TableCell>
-                                        <TableCell align="right">操作</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {accounts.map((row,id) => (
-                                        <TableRow key={id}>
-                                            <TableCell component="th" scope="row">
-                                                {row.Name}
-                                            </TableCell>
-                                            <TableCell align="right">{row.Password}</TableCell>
-                                            <TableCell align="right">{row.Root}</TableCell>
+                    {tab === 0 && (
+                        <div>
+                            <Alert severity="info">
+                                WebDAV的登陆用户名统一为：{user.user_name}{" "}
+                                ；密码为所创建账号的密码。
+                            </Alert>
+                            <TableContainer className={classes.tableContainer}>
+                                <Table
+                                    className={classes.table}
+                                    aria-label="simple table"
+                                >
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell>账号名称</TableCell>
                                             <TableCell align="right">
-                                                <TimeAgo
-                                                    datetime={row.CreatedAt}
-                                                    locale="zh_CN"
-                                                />
+                                                密码
                                             </TableCell>
                                             <TableCell align="right">
-                                                <IconButton size={"small"} onClick={()=>deleteAccount(id)}><Delete/></IconButton>
+                                                根目录
+                                            </TableCell>
+                                            <TableCell align="right">
+                                                创建日期
+                                            </TableCell>
+                                            <TableCell align="right">
+                                                操作
                                             </TableCell>
                                         </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                        <Button onClick={()=>setCreate(true)} className={classes.create} variant="contained" color="secondary">
-                            创建新账号
-                        </Button>
-                    </div>}
+                                    </TableHead>
+                                    <TableBody>
+                                        {accounts.map((row, id) => (
+                                            <TableRow key={id}>
+                                                <TableCell
+                                                    component="th"
+                                                    scope="row"
+                                                >
+                                                    {row.Name}
+                                                </TableCell>
+                                                <TableCell align="right">
+                                                    {row.Password}
+                                                </TableCell>
+                                                <TableCell align="right">
+                                                    {row.Root}
+                                                </TableCell>
+                                                <TableCell align="right">
+                                                    <TimeAgo
+                                                        datetime={row.CreatedAt}
+                                                        locale="zh_CN"
+                                                    />
+                                                </TableCell>
+                                                <TableCell align="right">
+                                                    <IconButton
+                                                        size={"small"}
+                                                        onClick={() =>
+                                                            deleteAccount(id)
+                                                        }
+                                                    >
+                                                        <Delete />
+                                                    </IconButton>
+                                                </TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                            <Button
+                                onClick={() => setCreate(true)}
+                                className={classes.create}
+                                variant="contained"
+                                color="secondary"
+                            >
+                                创建新账号
+                            </Button>
+                        </div>
+                    )}
+                    {tab === 1 && (
+                        <div>
+                            <Alert severity="info">
+                                为目录挂载存储策略后，在 WebDAV 中上传至此目录的新文件将会使用挂载的存储策略存储。复制、移动到此目录不会应用挂载的存储策略；挂载设置不会被子目录继承。
+                            </Alert>
+                            <TableContainer className={classes.tableContainer}>
+                                <Table
+                                    size="small"
+                                    className={classes.table}
+                                    aria-label="simple table"
+                                >
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell>目录名</TableCell>
+                                            <TableCell align="right">
+                                                挂载存储策略
+                                            </TableCell>
+                                            <TableCell align="right">
+                                                操作
+                                            </TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {folders.map((row, id) => (
+                                            <TableRow key={id}>
+                                                <TableCell
+                                                    component="th"
+                                                    scope="row"
+                                                >
+                                                    {row.name}
+                                                </TableCell>
+                                                <TableCell align="right">
+                                                    {row.policy_name}
+                                                </TableCell>
+                                                <TableCell align="right">
+                                                    <IconButton
+                                                        size={"small"}
+                                                        onClick={() =>
+                                                            deleteMount(id)
+                                                        }
+                                                    >
+                                                        <Delete />
+                                                    </IconButton>
+                                                </TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                            <Button
+                                onClick={() => setMount(true)}
+                                className={classes.create}
+                                variant="contained"
+                                color="secondary"
+                            >
+                                挂载新目录
+                            </Button>
+                        </div>
+                    )}
                 </div>
             </Paper>
         </div>
-    )
-
+    );
 }
