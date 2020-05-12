9019649b88a71d6002c40a6d698d30cab0284d5f
Tianhao Shi <zjsth92@gmail.com>
2020-05-11 22:11:47 -0700

    fix: dispatch change sub title

--- src/redux/viewUpdate/action.ts
+++ src/redux/viewUpdate/action.ts
@@ -10,9 +10,9 @@ ThunkAction<ACTION_CHANGE_SUBTITLE, any, any, any> => {
   return (dispatch, getState) => {
     const state = getState()
     document.title = (title === null || title === undefined) ? state.siteConfig.title : (title + " - " +state.siteConfig.title);
-    return {
+    return dispatch({
       type: "CHANGE_SUB_TITLE",
       title: title
-    }
+    })
   }
 }
