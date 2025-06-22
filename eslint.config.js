8598a56148fa0bd17c92d9cb006bc06335928c25
Aaron Liu <abslant.liu@gmail.com>
2025-06-22 11:09:43 +0800

    chore: husky hook for eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]);
