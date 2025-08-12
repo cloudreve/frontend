89d28f1a31879bbb511330522825c4220b029c27
Darren Yu <YDL@YUDONGLING.NET>
2025-08-12 11:12:04 +0800

    fix(ui): add padding to ListView, keep same with GridView

--- src/component/FileManager/Explorer/ListView/ListView.tsx
+++ src/component/FileManager/Explorer/ListView/ListView.tsx
@@ -81,6 +81,7 @@ const ListView = React.forwardRef(
           height: "100%",
           display: "flex",
           flexDirection: "column",
+          padding: 8,
         }}
       >
         <ListHeader commitColumnSetting={commitColumnSetting} setColumns={setColumns} columns={columns} />
