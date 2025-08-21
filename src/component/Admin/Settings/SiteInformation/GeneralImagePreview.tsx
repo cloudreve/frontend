f0ad9ff896d4e2c34ff22642f2b670b9bc02f9ae
Darren Yu <YDL@YUDONGLING.NET>
2025-08-21 09:55:38 +0800

    perf(setting): add debounce prop for logo setting preview loading optimization (#297)

--- src/component/Admin/Settings/SiteInformation/GeneralImagePreview.tsx
+++ src/component/Admin/Settings/SiteInformation/GeneralImagePreview.tsx
@@ -1,17 +1,28 @@
 import { Box } from "@mui/material";
-import React from "react";
+import { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useTheme } from "@mui/material/styles";
 
 export interface GeneralImagePreviewProps {
   src: string;
+  debounce?: number; // (可选) 防抖时间
 }
 
-const GeneralImagePreview = ({ src }: GeneralImagePreviewProps) => {
+const GeneralImagePreview = ({ src, debounce = 0 }: GeneralImagePreviewProps) => {
   const { t } = useTranslation("dashboard");
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const [debouncedSrc, setDebouncedSrc] = useState(src);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedSrc(src);
+    }, debounce);
+
+    return () => clearTimeout(handler);
+  }, [src, debounce]);
 
   return (
     <Box sx={{ mt: isMobile ? 0 : 3 }}>
@@ -25,7 +36,7 @@ const GeneralImagePreview = ({ src }: GeneralImagePreviewProps) => {
       >
         <Box
           component={"img"}
-          src={src}
+          src={debouncedSrc}
           sx={{
             display: "block",
             maxWidth: "100%",
